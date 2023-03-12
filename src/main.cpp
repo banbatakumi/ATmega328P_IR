654c80c505abf110164c4cbb45a4454acce3ac31
banbatakumi <bantech0210@gmail.com>
2023-03-12 13:59:46 +0900

    大会前

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #define PIN_6 7
 #define PIN_7 4
 #define IR_NUM 8
-#define READ_NUMBER_OF_TIME 500
+#define READ_NUMBER_OF_TIME 1000
 #define DISTANCE_RC 0.5
 
 uint8_t angle_plus, angle_minus;
@@ -20,6 +20,9 @@ float unit_vector_x[IR_NUM];
 float unit_vector_y[IR_NUM];
 
 void setup() {
+      DDRB = 0b00000000;   // ポートBを全てINPUT
+      DDRD = 0b00000000;   // ポートDを全てINPUT
+      /*
       pinMode(PIN_0, INPUT);
       pinMode(PIN_1, INPUT);
       pinMode(PIN_2, INPUT);
@@ -28,6 +31,7 @@ void setup() {
       pinMode(PIN_5, INPUT);
       pinMode(PIN_6, INPUT);
       pinMode(PIN_7, INPUT);
+      */
 
       Serial.begin(38400);
 
@@ -40,14 +44,14 @@ void setup() {
 void loop() {
       for (uint8_t count = 0; count < IR_NUM; count++) value[count] = 0;
       for (uint16_t count = 0; count < READ_NUMBER_OF_TIME; count++) {
-            value[0] += digitalRead(PIN_0);
-            value[1] += digitalRead(PIN_1);
-            value[2] += digitalRead(PIN_2);
-            value[3] += digitalRead(PIN_3);
-            value[4] += digitalRead(PIN_4);
-            value[5] += digitalRead(PIN_5);
-            value[6] += digitalRead(PIN_6);
-            value[7] += digitalRead(PIN_7);
+            value[0] += (PINB >> 1) & 0b00000001;   // digitalRead(PIN_0);
+            value[1] += (PIND >> 3) & 0b00000001;   // digitalRead(PIN_1);
+            value[2] += (PIND >> 6) & 0b00000001;   // digitalRead(PIN_2);
+            value[3] += (PIND >> 5) & 0b00000001;   // digitalRead(PIN_3);
+            value[4] += PINB & 0b00000001;   // digitalRead(PIN_4);
+            value[5] += (PIND >> 2) & 0b00000001;   // digitalRead(PIN_5);
+            value[6] += (PIND >> 7) & 0b00000001;   // digitalRead(PIN_6);
+            value[7] += (PIND >> 4) & 0b00000001;   // digitalRead(PIN_7);
       }
       for (uint8_t count = 0; count < IR_NUM; count++) value[count] = (READ_NUMBER_OF_TIME - value[count]) / (READ_NUMBER_OF_TIME / 100);
 
